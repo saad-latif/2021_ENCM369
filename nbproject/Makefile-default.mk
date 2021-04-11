ebd4059299e1e3055943216949d9b57244a7ebdc
saad-latif <saadlatif30@gmail.com>
2021-04-11 08:52:54 +0300

    push 2

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED="../../../Desktop/PIC activities/ProjectTemplate/encm369_pic18.c" "../../../Desktop/PIC activities/ProjectTemplate/main.c" "../../../Desktop/PIC activities/ProjectTemplate/user_app.c" ../../../Desktop/PIC_7/encm369_pic18.c
+SOURCEFILES_QUOTED_IF_SPACED="../../../Desktop/PIC activities/ProjectTemplate/main.c" "../../../Desktop/PIC activities/ProjectTemplate/user_app.c" ../../../Desktop/PIC_7/encm369_pic18.c interrupts.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1 ${OBJECTDIR}/_ext/1733884649/main.p1 ${OBJECTDIR}/_ext/1733884649/user_app.p1 ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1.d ${OBJECTDIR}/_ext/1733884649/main.p1.d ${OBJECTDIR}/_ext/1733884649/user_app.p1.d ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1733884649/main.p1 ${OBJECTDIR}/_ext/1733884649/user_app.p1 ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1 ${OBJECTDIR}/interrupts.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1733884649/main.p1.d ${OBJECTDIR}/_ext/1733884649/user_app.p1.d ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1.d ${OBJECTDIR}/interrupts.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1 ${OBJECTDIR}/_ext/1733884649/main.p1 ${OBJECTDIR}/_ext/1733884649/user_app.p1 ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1
+OBJECTFILES=${OBJECTDIR}/_ext/1733884649/main.p1 ${OBJECTDIR}/_ext/1733884649/user_app.p1 ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1 ${OBJECTDIR}/interrupts.p1
 
 # Source Files
-SOURCEFILES=../../../Desktop/PIC activities/ProjectTemplate/encm369_pic18.c ../../../Desktop/PIC activities/ProjectTemplate/main.c ../../../Desktop/PIC activities/ProjectTemplate/user_app.c ../../../Desktop/PIC_7/encm369_pic18.c
+SOURCEFILES=../../../Desktop/PIC activities/ProjectTemplate/main.c ../../../Desktop/PIC activities/ProjectTemplate/user_app.c ../../../Desktop/PIC_7/encm369_pic18.c interrupts.c
 
 
 
@@ -94,14 +94,6 @@ MP_PROCESSOR_OPTION=18F27Q43
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1: ../../../Desktop/PIC\ activities/ProjectTemplate/encm369_pic18.c  nbproject/Makefile-${CND_CONF}.mk 
-	@${MKDIR} "${OBJECTDIR}/_ext/1733884649" 
-	@${RM} ${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1 "../../../Desktop/PIC activities/ProjectTemplate/encm369_pic18.c" 
-	@-${MV} ${OBJECTDIR}/_ext/1733884649/encm369_pic18.d ${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
 ${OBJECTDIR}/_ext/1733884649/main.p1: ../../../Desktop/PIC\ activities/ProjectTemplate/main.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}/_ext/1733884649" 
 	@${RM} ${OBJECTDIR}/_ext/1733884649/main.p1.d 
@@ -126,15 +118,15 @@ ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1: ../../../Desktop/PIC_7/encm369_pi
 	@-${MV} ${OBJECTDIR}/_ext/1174896224/encm369_pic18.d ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-else
-${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1: ../../../Desktop/PIC\ activities/ProjectTemplate/encm369_pic18.c  nbproject/Makefile-${CND_CONF}.mk 
-	@${MKDIR} "${OBJECTDIR}/_ext/1733884649" 
-	@${RM} ${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1 "../../../Desktop/PIC activities/ProjectTemplate/encm369_pic18.c" 
-	@-${MV} ${OBJECTDIR}/_ext/1733884649/encm369_pic18.d ${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1733884649/encm369_pic18.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+${OBJECTDIR}/interrupts.p1: interrupts.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/interrupts.p1.d 
+	@${RM} ${OBJECTDIR}/interrupts.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/interrupts.p1 interrupts.c 
+	@-${MV} ${OBJECTDIR}/interrupts.d ${OBJECTDIR}/interrupts.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/interrupts.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+else
 ${OBJECTDIR}/_ext/1733884649/main.p1: ../../../Desktop/PIC\ activities/ProjectTemplate/main.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}/_ext/1733884649" 
 	@${RM} ${OBJECTDIR}/_ext/1733884649/main.p1.d 
@@ -158,6 +150,14 @@ ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1: ../../../Desktop/PIC_7/encm369_pi
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1 ../../../Desktop/PIC_7/encm369_pic18.c 
 	@-${MV} ${OBJECTDIR}/_ext/1174896224/encm369_pic18.d ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/_ext/1174896224/encm369_pic18.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/interrupts.p1: interrupts.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/interrupts.p1.d 
+	@${RM} ${OBJECTDIR}/interrupts.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/interrupts.p1 interrupts.c 
+	@-${MV} ${OBJECTDIR}/interrupts.d ${OBJECTDIR}/interrupts.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/interrupts.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
